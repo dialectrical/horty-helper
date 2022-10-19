385e9390feae4a046ab3433309504021150b7a55
dialectrical <michael.camele@gmail.com>
2022-10-18 18:04:23 -0700

    added set_dict(x) to hortyHelper.py

--- hortyHelper.py
+++ hortyHelper.py
@@ -6,6 +6,9 @@ class hortyHelper:
     dict_subset = []
     output_matrix = []
 
+    def set_dict(x):
+        dict = x
+
     def make_subset():
         for i in range(len(list(dict)) + 1):
             for subset in itertools.combinations(list(dict), i):
