3ea9f3f3fb5fcdec47851fdbccbe62e1f41b72d3
dialectrical <michael.camele@gmail.com>
2022-10-18 19:22:49 -0700

    began adding run_scenario(), added consistent_check() helper

--- hortyHelper.py
+++ hortyHelper.py
@@ -16,7 +16,25 @@ class hortyHelper:
             for subset in itertools.combinations(list(self.scenario_dict), i):
                 self.dict_subset.append(subset)
 
+    def run_scenario(self, scenario):
+        if len(self.dict_suubset) == 0:
+            make_subset()
 
+        def consistent_check():
+            seen = set()
+            conflict = 0
+            for i in self.dict_subset[scenario]:
+                if self.scenario_dict[i][0] is not '!' and '!' + self.scenario_dict[i] in seen:
+                    conflict = 1
+                elif self.scenario_dict[i][0] is '!' and self.scenario_dict[i][1] in seen:
+                    conflict = 1
+                seen.add(self.scenario_dict[i])
+            if conflict is not 0:
+                self.output_matrix[scenario].append('Not Consistent')
+            else:
+                self.output_matrix[scenario].append('Consistent')
+
+        self.output_matrix.append([self.dict_subset[scenario]])
 "simple tests"
 tester = hortyHelper()
 tester.set_dict({"socrates" : "man", "man" : "mortal"})
