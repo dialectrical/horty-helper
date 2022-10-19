a906bbd490a51370f06586ca4f04cd5846987f3c
dialectrical <michael.camele@gmail.com>
2022-10-18 18:00:39 -0700

    added make_subset() to hortyHelper.py

--- hortyHelper.py
+++ hortyHelper.py
@@ -5,3 +5,8 @@ class hortyHelper:
     dict = {}
     dict_subset = []
     output_matrix = []
+
+    def make_subset():
+        for i in range(len(list(dict)) + 1):
+            for subset in itertools.combinations(list(dict), i):
+                dict_subset.append(subset)
