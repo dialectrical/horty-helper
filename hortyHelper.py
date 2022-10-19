30872c03dfb572a9380b771e4716e7214fd46c7f
dialectrical <michael.camele@gmail.com>
2022-10-18 18:37:06 -0700

    added scenario_index() and test

--- hortyHelper.py
+++ hortyHelper.py
@@ -4,6 +4,7 @@ class hortyHelper:
     world = ""
     scenario_dict = {}
     dict_subset = []
+    scenario_index = {}
     output_matrix = []
 
     def set_dict(self, x):
@@ -12,13 +13,17 @@ class hortyHelper:
         self.scenario_dict = x
 
     def make_subset(self):
-        print(self.scenario_dict)
         for i in range(len(list(self.scenario_dict)) + 1):
             for subset in itertools.combinations(list(self.scenario_dict), i):
                 self.dict_subset.append(subset)
-        print(self.dict_subset)
+
+    def scenario_index():
+        for i, l in list(self.scenario_dict):
+            self.dict_index[l] = i
+        print(self.dict_index)
 
 "simple tests"
 tester = hortyHelper()
 tester.set_dict({"socrates" : "man", "man" : "mortal"})
 tester.make_subset()
+tester.dict_index()
