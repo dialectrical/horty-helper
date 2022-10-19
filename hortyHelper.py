253a3ab045c4be0ba7448ff43ff0dbabcf14a894
dialectrical <michael.camele@gmail.com>
2022-10-18 18:13:54 -0700

    added type checking to set_dict(x), renamed dict to scenario_dict

--- hortyHelper.py
+++ hortyHelper.py
@@ -2,12 +2,14 @@ import itertools
 
 class hortyHelper:
     world = ""
-    dict = {}
+    scenario_dict = {}
     dict_subset = []
     output_matrix = []
 
     def set_dict(x):
-        dict = x
+        if type(x) is not dict:
+            return print('Error: input must be a dictionary.')
+        scenario_dict = x
 
     def make_subset():
         for i in range(len(list(dict)) + 1):
